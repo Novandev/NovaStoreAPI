99601dc3464be027039af7d4c03c7b51082e6775
Novandev <donovan.adams@students.makeschool.com>
2019-03-03 19:54:56 -0800

    tryng this again after a .env commit

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
+
 import (
-"fmt"
-"github.com/joho/godotenv"
-"log"
-"os"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
+
+
+type User struct {
+	gorm.Model
+	Username string
+	Password string
+}
 
 
 func main(){
@@ -12,6 +21,11 @@ authErr := godotenv.Load()
 if authErr != nil {
 log.Fatal("Error loading .env file")
 }
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
 accessKey := os.Getenv("ACCESS")
 secretKey := os.Getenv("SECRET")
 format := "\nAccess: %s\nSecret: %s\n"
