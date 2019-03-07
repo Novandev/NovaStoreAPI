5e2d988827d0d84ee936b12a56e30f30bf269b6c
Novandev <donovan.adams@students.makeschool.com>
2019-03-07 13:03:41 -0800

    added.env

--- main.go
+++ main.go
@@ -47,6 +47,6 @@ log.Fatal(authErr.Error())
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":3000"))
+	e.Logger.Fatal(e.Start(":"))
 
 }
