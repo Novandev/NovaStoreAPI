c7dd741db195308e35765dac59a72ef784879c7a
Novandev <donovan.adams@students.makeschool.com>
2019-03-07 08:32:45 -0800

    herou push

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -21,11 +23,12 @@ authErr := godotenv.Load()
 if authErr != nil {
 log.Fatal("Error loading .env file")
 }
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
+	//db, err := gorm.Open("sqlite3", "test.db")
+	//if err != nil {
+	//	panic("failed to connect database")
+	//}
+	//defer db.Close()
+
 accessKey := os.Getenv("ACCESS")
 secretKey := os.Getenv("SECRET")
 format := "\nAccess: %s\nSecret: %s\n"
@@ -34,6 +37,16 @@ _, authErr = fmt.Printf(format, accessKey, secretKey)
 if authErr != nil {
 log.Fatal(authErr.Error())
 }
+	e := echo.New()
+	//e.POST("/files", saveFile)
+	//e.GET("/files/:id", getFile)
+	//e.DELETE("/files/:id", deleteFile)
+	//e.POST("/sign-up", newUser)
+	//e.POST("/sign-in", signinUser)
 
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":3000"))
 
 }
