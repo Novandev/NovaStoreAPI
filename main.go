2ee58e7e1af852d5505eb5b228b20ee8285d02c9
Novandev <donovan.adams@students.makeschool.com>
2019-03-08 10:39:48 -0800

    addig all the stff fromanother projec siince it broke when i moved into go foler...idkwhy

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"go.mongodb.org/mongo-driver/mongo"
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
@@ -17,7 +21,7 @@ type User struct {
 	Password string
 }
 
-
+//noinspection ALL
 func main(){
 authErr := godotenv.Load()
 if authErr != nil {
@@ -44,9 +48,79 @@ log.Fatal(authErr.Error())
 		log.Fatal("$PORT must be set")
 	}
 	e := echo.New()
-	//e.POST("/files", saveFile)
-	//e.GET("/files/:id", getFile)
-	//e.DELETE("/files/:id", deleteFile)
+
+// AWS Section
+
+
+	// Open an AWS session in order to get access to buckets
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-1")})
+	uploader := s3manager.NewUploader(sess)
+
+
+
+	//
+	//// Echo fucntion sections
+	//func getFile(c echo.Context) error {
+	//
+	//	// User ID from path `users/:id`
+	//	userId := c.Param("id")
+	//	// file name from
+	//	file := c.Param("File")
+	//
+	//	f, err  := os.Open(file)
+	//	if err != nil {
+	//	return fmt.Errorf("failed to open file %q, %v", filename, err)
+	//}
+	//
+	//	return c.String(http.StatusOK, f)
+	//}
+	//
+	//
+	//
+	//
+	//func getAllFiles(c echo.Context) error {
+	//	// User ID from path `users/:id`
+	//	userId := c.Param("id")
+	//	return c.String(http.StatusOK, id)
+	//}
+	//
+	//
+	//
+	//func saveFile(c echo.Context) error {
+	//	// Get name
+	//	userId := c.FormValue("userId")
+	//	// Get avatar
+	//	CSV, err := c.FormFile("CSV")
+	//	if err != nil {
+	//	return err
+	//}
+	//
+	//	// Source
+	//	src, err := avatar.Open()
+	//	if err != nil {
+	//	return err
+	//}
+	//	defer src.Close()
+	//
+	//	// Destination
+	//	dst, err := os.Create(avatar.Filename)
+	//	if err != nil {
+	//	return err
+	//}
+	//	defer dst.Close()
+	//
+	//	// Copy
+	//	if _, err = io.Copy(dst, src); err != nil {
+	//	return err
+	//}
+	//
+	//	return c.HTML(http.StatusOK, "<b>Thank you! " + name + "</b>")
+	//}
+	//
+	//e.POST("user/:id/files", saveFile)
+	//e.GET("user/:id/files/:id", getFile)
+	//e.GET("user/:id/files/all", getAllFiles)
+	//e.DELETE("user/:id/files/:id", deleteFile)
 	//e.POST("/sign-up", newUser)
 	//e.POST("/sign-in", signinUser)
 
