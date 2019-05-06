86e9ea448bb33562962862a34d3ab120a29bbcaa
novandev <donovan.adams@students.makeschool.com>
2019-05-06 09:51:08 -0700

    added authentiation to CLI now hooking up to database

--- main.go
+++ main.go
@@ -7,14 +7,17 @@ import (
 	"os"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/imroc/req"
 	"github.com/joho/godotenv"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/middleware/logger"
 	"github.com/kataras/iris/middleware/recover"
+	uuid "github.com/nu7hatch/gouuid"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 	"gopkg.in/mgo.v2/bson"
 	//"strings"
 )
@@ -65,10 +68,11 @@ func main() {
 	app.Get("/", func(context iris.Context) {
 		context.WriteString("NovaStore")
 	})
+
+	// ---------- REGISTER ROUTE ----------//
 	app.Post("/register", func(ctx iris.Context) {
 		var u User
 		err := ctx.ReadJSON(&u)
-
 		if err != nil {
 			ctx.WriteString(err.Error())
 			ctx.StatusCode(iris.StatusBadRequest)
@@ -77,6 +81,9 @@ func main() {
 		}
 		// give the comment a unique ID and set the time
 		u.CreatedAt = time.Now()
+		u.HashUserPassword()
+		u.GenerateAuthToken()
+		u.NewId()
 		_, resErr := UserCollection.InsertOne(dbCtx, u)
 		if resErr != nil {
 			panic(resErr)
@@ -87,10 +94,32 @@ func main() {
 		}
 		//ctx.Application().Logger().Infof("received %#+v", u.Email)
 		//ctx.Application().Logger().Infof("received %#+v", id)
-		response := map[string]string{"status": "200", "Email Registered": u.Email}
+		response := map[string]string{"Status": "200", "Email Registered": u.Email, "Auth_token": u.Auth_token, "ID": u.ID}
 		ctx.JSON(response)
 	})
 
+	// ---------- LOGIN ROUTE ----------//
+
+	app.Post("/login", func(ctx iris.Context) {
+		var u User
+		err := ctx.ReadJSON(&u)
+		if err != nil {
+			ctx.WriteString(err.Error())
+			ctx.StatusCode(iris.StatusBadRequest)
+			return
+		}
+
+		res, err := UserCollection.Find(dbCtx, bson.M{"email": u.Email, "password": u.Password})
+		if err != nil {
+			ctx.WriteString(err.Error())
+			ctx.StatusCode(iris.StatusUnauthorized)
+			return
+		}
+		response := map[string]string{"Status": "200", "Email": u.Email, "Auth_token": u.Auth_token, "ID": u.ID}
+		ctx.JSON(response)
+	})
+
+	// ---------- Make Model ----------//
 	app.Post("/make-model", func(ctx iris.Context) {
 		file, info, err := ctx.FormFile("file")
 		fmt.Println(info)
@@ -129,34 +158,64 @@ func main() {
 		defer file.Close()
 		// fmt.Println(info)
 		// defer out.Close()
-	})
-
-	app.Post("/login", func(ctx iris.Context) {
-		var u User
-		err := ctx.ReadJSON(&u)
-		if err != nil {
-			ctx.WriteString(err.Error())
-			ctx.StatusCode(iris.StatusBadRequest)
-			return
-		}
-		res, err := UserCollection.Find(dbCtx, bson.M{"email": u.Email, "password": u.Password})
-		if err != nil {
-			ctx.WriteString(err.Error())
-			ctx.StatusCode(iris.StatusBadRequest)
-			return
-		}
-		fmt.Println(res)
+		return
 	})
 
 	app.Run(iris.Addr(":" + port))
 }
 
+// USER AND AUTHENTICATION SECTION.... TO BE MOVED Asap
+var jwtKey = []byte(os.Getenv("JWT_SECRET"))
+
 type (
 	User struct {
-		ID         bson.ObjectId `json:"id" bson:"_id,omitempty"`
-		Email      string        `json:"email"`
-		Password   string        `json:"password"`
-		Auth_token string        `json:"auth_token"`
-		CreatedAt  time.Time     `json:"CreatedAt"`
+		ID         string    `json:"id"`
+		Email      string    `json:"email"`
+		Password   string    `json:"password"`
+		Auth_token string    `json:"auth_token"`
+		Model      string    `json:"model"`
+		Endpoint   string    `json:"endpoint"`
+		CreatedAt  time.Time `json:"CreatedAt"`
 	}
 )
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func (u *User) HashUserPassword() {
+	password, _ := HashPassword(u.Password)
+	u.Password = password
+}
+
+func (u *User) NewId() {
+	id, _ := uuid.NewV4()
+	u.ID = id.String()
+}
+
+// Helper string Claims for jwt
+type Claims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
+
+func (u *User) GenerateAuthToken() {
+
+	claims := &Claims{
+		Email:          u.Email,
+		StandardClaims: jwt.StandardClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, _ := token.SignedString(jwtKey)
+
+	u.Auth_token = tokenString
+}
